520442428218facfe2bfb1d60719eb275e4d9108
Agnieszka <agnieszka.zambrzycka@gmail.com>
2026-05-11 19:57:12 +0200

    Prevent duplicate Prometheus metric registration

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -1,9 +1,36 @@
-from prometheus_client import Counter, Histogram
+from prometheus_client import Counter, Histogram, REGISTRY
 
-REQUEST_COUNT = Counter("requests_total", "Total number of API requests")
 
-REQUEST_LATENCY = Histogram("request_latency_seconds", "API request latency in seconds")
+def metric_exists(name: str) -> bool:
+    return name in REGISTRY._names_to_collectors
 
-PREDICTION_LATENCY = Histogram(
-    "prediction_latency_seconds", "Prediction latency in seconds"
-)
+
+if metric_exists("salary_prediction_requests"):
+    REQUEST_COUNT = REGISTRY._names_to_collectors["salary_prediction_requests"]
+else:
+    REQUEST_COUNT = Counter(
+        "salary_prediction_requests",
+        "Total number of API requests",
+    )
+
+
+if metric_exists("salary_prediction_request_latency_seconds"):
+    REQUEST_LATENCY = REGISTRY._names_to_collectors[
+        "salary_prediction_request_latency_seconds"
+    ]
+else:
+    REQUEST_LATENCY = Histogram(
+        "salary_prediction_request_latency_seconds",
+        "API request latency in seconds",
+    )
+
+
+if metric_exists("salary_prediction_inference_latency_seconds"):
+    PREDICTION_LATENCY = REGISTRY._names_to_collectors[
+        "salary_prediction_inference_latency_seconds"
+    ]
+else:
+    PREDICTION_LATENCY = Histogram(
+        "salary_prediction_inference_latency_seconds",
+        "Prediction latency in seconds",
+    )
