2e5c31cd19ecdd93bf3cedc213f3cba272d36c60
Agnieszka <agnieszka.zambrzycka@gmail.com>
2026-05-12 01:28:38 +0200

    Remove unused file

--- src/scraping/run_scrapper.py
+++
@@ -1,6 +0,0 @@
-from scrapping.scrapper import fetch_job_listings
-
-url = "https://nofluffjobs.com/pl/jobs?criteria=python"  # exemplary filter
-df = fetch_job_listings(url)
-print(df.head())
-df.to_csv("data/raw/jobs.csv", index=False)
